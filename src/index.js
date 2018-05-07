5f6092179dc6feadf5b95777a5ac54086b88939d
dengdong <853315284@qq.com>
2018-05-07 16:16:37 +0800

    change file name.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import spaceBlock from './spaceBlock.js';
+import SpaceBlock from './spaceBlock.js';
 
 // 生成棋盘
 const row = col = 19;
+let checkerboard = [];
 for (let i = 0; i < row; i++) {
+  let line = [];
   for (let j = 0; j < col; j++) {
     if (i % 2 == 0) { // 偶数行
       if (j % 2 == 0) { // 偶数列
         // 生成木板与木板之间的空隙块
-
+        line.push(new SpaceBlock(i, j));
       } else { // 奇数列
 
       }
