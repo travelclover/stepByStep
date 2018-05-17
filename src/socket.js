acb3a23386de237a7a4040b15c78481c2c98fe3f
dengdong <853315284@qq.com>
2018-05-17 14:47:17 +0800

    修改服务地址为线上地址

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001/stepByStep', {
+const socket = io('http://118.126.110.61:3001/stepByStep', {
   reconnection: true, // 是否自动重连
 });
 socket.on('news', function (data) {
